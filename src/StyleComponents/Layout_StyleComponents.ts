f03e6c74241314bff93a0e555a0678655d54509e
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-26 23:45:20 -0500

    Aprendiendo ts

--- src/StyleComponents/Layout_StyleComponents.ts
+++ src/StyleComponents/Layout_StyleComponents.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const LayoutContainer = styled.div""
+export const LayoutContainer = styled.div`
+width: 70vw;
+background: white;
+border-radius: 5px;
+
+@media only screen and (max-width: 1176px){
+    padding:12xp;
+}
+`;
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
 import { People } from "@/data/people";
+import { Persone } from "@/models/people";
+import { Checkbox, speedDialActionClasses } from "@mui/material";
 
 export interface HomeInterface {
   pageSize: Number;
 }
 
 export default function Home<HomeInterface>() {
+  const [selectedPeople, setSelectedPeople] = useState<Persone[]>([]);
   const pageSize = 5;
+
+  const findPerson = (person : Persone) => !!selectedPeople.find((p) => p.id === person.id);
+  const filterPerson = (person : Persone) => selectedPeople.filter((p) => p.id   person.id)
+
+  const handleChange = (person: Persone) => {
+    setSelectedPeople(findPerson(person) ? filterPerson(person) : [...selectedPeople, person]);
+  };
+
   const colums = [
+    {
+      field: "Action",
+      headerName: "",
+      Width: 50,
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          {
+            <Checkbox
+              size="small"
+              checked={findPerson(params.row.id)}
+              onChange={() => handleChange(params.row.id)}
+            />
+          }
+        </>
+      ),
+    },
     {
       field: "name",
       headerName: "Name",
@@ -21,25 +48,25 @@ export default function Home<HomeInterface>() {
       field: "company",
       headerName: "Company",
       flex: 1,
-          renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-    },  
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
     {
       field: "category",
       headerName: "Categories",
       flex: 1,
-          renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
     },
   ];
   return (
-      <DataGrid
-        rows={People}
-        columns={colums}
-        disableColumnSelector
-        disableSelectionOnClick
-        autoHeight
-        pageSize={pageSize}
-        rowsPerPageOptions={[pageSize]}
-        getRowId ={(row : any) => row.id}
-      />
+    <DataGrid
+      rows={People}
+      columns={colums}
+      disableColumnSelector
+      disableSelectionOnClick
+      autoHeight
+      pageSize={pageSize}
+      rowsPerPageOptions={[pageSize]}
+      getRowId={(row: any) => row.id}
+    />
   );
 }
